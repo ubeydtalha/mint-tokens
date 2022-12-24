a71574a4ad418f43e914fa2121687ccbafcf9c31
Ubeyd Talha Alkan <62707118+ubeydtalha@users.noreply.github.com>
2022-12-24 22:31:51 +0300

    save

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,26 @@ import {
   keypairIdentity,
   bundlrStorage,
   toMetaplexFile,
-  
+  Account,
+
 } from "@metaplex-foundation/js"
 
-import { 
+import {
   DataV2,
   createCreateMetadataAccountV2Instruction,
   createUpdateMetadataAccountV2Instruction,
- } from "@metaplex-foundation/mpl-token-metadata"
+} from "@metaplex-foundation/mpl-token-metadata"
 
 
 import * as fs from "fs"
 import { findMetadataPda } from "@metaplex-foundation/js/dist/types/plugins"
+import { PublicKey } from '@solana/web3.js';
 
+/** Address of the SPL Token program */
+const TOKEN_PROGRAM_ID = new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
+
+/** Address of the SPL Associated Token Account program */
+const ASSOCIATED_TOKEN_PROGRAM_ID = new PublicKey('ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL');
 
 async function createNewMint(
   connection: web3.Connection,
@@ -37,7 +44,7 @@ async function createNewMint(
     decimals,
   );
 
-  console.log("Token mint created:", tokenMint);
+  // console.log("Token mint created:", tokenMint);
   console.log(
     `Token Mint: https://explorer.solana.com/address/${tokenMint}?cluster=devnet`
   );
@@ -54,31 +61,52 @@ async function createTokenAccount(
   owner: web3.PublicKey
 ) {
 
+  const associatedToken = await token.getAssociatedTokenAddress(
+    mint,
+    owner,
+  );
+
+
+  let tokenAccount: token.Account;
+
+
   try {
-    
-    const tokenAccount = await token.getTokenAccountInfo(
+
+    tokenAccount = await token.getAccount(
       connection,
-      owner
+      associatedToken,
     )
-    console.log("Token account found:", tokenAccount);
-    return tokenAccount
+
+    // console.log("Token account found:", tokenAccount);
 
   } catch (error) {
-    
-    console.log("Token account not found:", error);
+    if (error instanceof token.TokenAccountNotFoundError || error instanceof token.TokenInvalidAccountOwnerError) {
+      // As this isn't atomic, it's possible others can create associated accounts meanwhile.
+      try {
+        const transaction = new web3.Transaction().add(
+          token.createAssociatedTokenAccountInstruction(
+            payer.publicKey,
+            associatedToken,
+            owner,
+            mint,
+          )
+        );
 
-    const tokenAccount = await token.createAssociatedTokenAccount(
-      connection,
-      payer,
-      owner,
-      mint
-    )
+        await web3.sendAndConfirmTransaction(connection, transaction, [payer]);
+      } catch (error: unknown) {
+        // Ignore all errors; for now there is no API-compatible way to selectively ignore the expected
+        // instruction error if the associated account exists already.
+      }
 
+      // Now this should always succeed
+      tokenAccount = await token.getAccount(connection, associatedToken,);
+    } else {
+      throw error;
+    }
   }
 
-  console.log(
-      `Token Account: https://explorer.solana.com/address/${tokenAccount.address}?cluster=devnet`
-  )
+  if (!tokenAccount.mint.equals(mint)) throw new token.TokenInvalidMintError();
+  if (!tokenAccount.owner.equals(owner)) throw new token.TokenInvalidOwnerError();
 
   return tokenAccount
 }
@@ -93,7 +121,7 @@ async function mintTokens(
 
   const mintInfo = await token.getMint(connection, mint);
 
-  console.log("Mint info:", mintInfo);
+  // console.log("Mint info:", mintInfo);
 
   const transactionSignature = await token.mintTo(
     connection,
@@ -104,7 +132,7 @@ async function mintTokens(
     amount * 10 ** mintInfo.decimals,
   )
 
-  console.log("Transaction signature:", transactionSignature);
+  // console.log("Transaction signature:", transactionSignature);
 
   console.log(
     `Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
@@ -123,7 +151,7 @@ async function transferTokens(
   amount: number,
   mint: web3.PublicKey,
 ) {
-  
+
   const mintInfo = await token.getMint(connection, mint);
 
   console.log("Mint info:", mintInfo);
@@ -149,8 +177,8 @@ async function transferTokens(
 async function burnTokens(
   connection: web3.Connection,
   payer: web3.Keypair,
-  account : web3.PublicKey,
-  mint : web3.PublicKey,
+  account: web3.PublicKey,
+  mint: web3.PublicKey,
   owner: web3.Keypair,
   amount: number,
 ) {
@@ -177,47 +205,47 @@ async function burnTokens(
 
 async function createTokenMetadata(
   connection: web3.Connection,
-  metaplex : Metaplex,
-  mint : web3.PublicKey,
-  user : web3.Keypair,
-  name : string,
-  symbol : string,
-  description : string,
+  metaplex: Metaplex,
+  mint: web3.PublicKey,
+  user: web3.Keypair,
+  name: string,
+  symbol: string,
+  description: string,
 
-){
+) {
 
   // file to buffer
 
   const buffer = fs.readFileSync("assets/lama.png")
 
   // buffer to metaplex file
-  const file = toMetaplexFile(buffer , "lama.png")
+  const file = toMetaplexFile(buffer, "lama.png")
 
-   // upload image and get image uri
+  // upload image and get image uri
   const imageUri = await metaplex.storage().upload(file)
   console.log("Image URI:", imageUri);
-  
+
 
   // upload metadata and get metadata uri (off chain metadata)
   const { uri } = await metaplex.nfts().uploadMetadata({
-    name : name,
-    description : description,
-    image : imageUri,
+    name: name,
+    description: description,
+    image: imageUri,
   })
 
   // get metadata account address
-  const metadataPDA = await metaplex.nfts().pdas().metadata({mint: mint})
+  const metadataPDA = await metaplex.nfts().pdas().metadata({ mint: mint })
 
   // onchain metadata format
   const tokenMetadata = {
-    name : name,
-    symbol : symbol,
-    uri : uri,
-    sellerFeeBasisPoints : 0,
-    creators : null,
-    collection : null,
-    uses : null,
-  
+    name: name,
+    symbol: symbol,
+    uri: uri,
+    sellerFeeBasisPoints: 0,
+    creators: null,
+    collection: null,
+    uses: null,
+
   } as DataV2
 
   // transaction to create metadata account
@@ -238,7 +266,7 @@ async function createTokenMetadata(
       }
     )
   )
-  
+
   // send transaction
   const transactionSignature = await connection.sendTransaction(
     transaction,
@@ -255,59 +283,59 @@ async function createTokenMetadata(
 
 async function updateTokenMetadata(
   connection: web3.Connection,
-  metaplex : Metaplex,
-  mint : web3.PublicKey,
-  user : web3.Keypair,
-  name : string,
-  symbol : string,
-  description : string,
-){
+  metaplex: Metaplex,
+  mint: web3.PublicKey,
+  user: web3.Keypair,
+  name: string,
+  symbol: string,
+  description: string,
+) {
   // file to buffer
 
   const buffer = fs.readFileSync("assets/lama.png")
 
   // buffer to metaplex file
-  const file = toMetaplexFile(buffer , "lama.png")
+  const file = toMetaplexFile(buffer, "lama.png")
 
-   // upload image and get image uri
+  // upload image and get image uri
   const imageUri = await metaplex.storage().upload(file)
   console.log("Image URI:", imageUri);
-  
+
 
   // upload metadata and get metadata uri (off chain metadata)
   const { uri } = await metaplex.nfts().uploadMetadata({
-    name : name,
-    description : description,
-    image : imageUri,
+    name: name,
+    description: description,
+    image: imageUri,
   })
 
   // get metadata account address
-  const metadataPDA = await metaplex.nfts().pdas().metadata({mint: mint})
+  const metadataPDA = await metaplex.nfts().pdas().metadata({ mint: mint })
 
   // onchain metadata format
   const tokenMetadata = {
-    name : name,
-    symbol : symbol,
-    uri : uri,
-    sellerFeeBasisPoints : 0,
-    creators : null,
-    collection : null,
-    uses : null,
-  
+    name: name,
+    symbol: symbol,
+    uri: uri,
+    sellerFeeBasisPoints: 0,
+    creators: null,
+    collection: null,
+    uses: null,
+
   } as DataV2
 
   const transaction = new web3.Transaction().add(
     createUpdateMetadataAccountV2Instruction(
       {
-        metadata : metadataPDA,
-        updateAuthority : user.publicKey,
+        metadata: metadataPDA,
+        updateAuthority: user.publicKey,
       },
       {
-        updateMetadataAccountArgsV2 : {
-          data : tokenMetadata,
-          updateAuthority : user.publicKey,
-          primarySaleHappened : true,
-          isMutable : true,
+        updateMetadataAccountArgsV2: {
+          data: tokenMetadata,
+          updateAuthority: user.publicKey,
+          primarySaleHappened: true,
+          isMutable: true,
         }
       }
     )
@@ -317,25 +345,25 @@ async function updateTokenMetadata(
 
 async function main() {
   const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
-  console.log("Connection to cluster established:", connection);
-  
+  console.log("Connection to cluster established:");
+
   const user = await initializeKeypair(connection)
 
   const mint = await createNewMint(connection, user, user.publicKey, user.publicKey, 2);
 
   const tokenAccount = await createTokenAccount(connection, user, mint, user.publicKey);
 
-  await mintTokens(connection, user, mint, tokenAccount.address , user, 100);
+  await mintTokens(connection, user, mint, tokenAccount.address, user, 100);
 
   console.log("PublicKey:", user.publicKey.toBase58())
 
   const receiver = web3.Keypair.generate().publicKey
-    
+
   const receiverTokenAccount = await createTokenAccount(
-      connection,
-      user,
-      mint,
-      receiver
+    connection,
+    user,
+    mint,
+    receiver
   )
 
   await transferTokens(
@@ -347,12 +375,12 @@ async function main() {
     50,
     mint
   )
-  
+
   await burnTokens(connection, user, tokenAccount.address, mint, user, 25)
-  
-  
-  const MINT_ADDRESS = "95ahsSaZrYkHKiocMgEoSwA1jtxBiB7iiW14yJKJ4xeY" 
-  
+
+
+  const MINT_ADDRESS = mint.toBase58()
+
   const metaplex = Metaplex.make(connection)
     .use(keypairIdentity(user))
     .use(
@@ -362,18 +390,18 @@ async function main() {
         timeout: 60000,
       })
     )
-    
+
   // Calling the token 
   await createTokenMetadata(
     connection,
     metaplex,
     new web3.PublicKey(MINT_ADDRESS),
     user,
-    "Rise",
-    "RISE",
+    "RiseV1",
+    "Risev1",
     "The first Rise in there and start of universe.",
   )
-    
+
 
 }
 
